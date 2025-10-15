9a4f753d7dece445b56fc59befc3560f45d3310c
벨라 <icomksh@gmail.com>
2025-10-15 17:54:54 +0900

    [FEAT/#5] 약관 동의 화면 제작

--- app/src/main/java/com/example/momolabfe/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/momolabfe/ui/auth/LoginFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.momolabfe.R
-import com.example.momolabfe.databinding.ActivityLoginBinding
-import com.example.momolabfe.databinding.FragmentAgreementBinding
 import com.example.momolabfe.databinding.FragmentLoginBinding
 import com.example.momolabfe.ui.onboarding.AgreementFragment
 
@@ -29,18 +25,9 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupUI()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        _binding = FragmentLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         binding.kakaoLoginBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, AgreementFragment())
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AgreementFragment())
                 .addToBackStack(null)
                 .commit()
         }
