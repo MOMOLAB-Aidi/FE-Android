e5eace072409a90f671a49a71b55b62afeff68d7
벨라 <icomksh@gmail.com>
2025-10-21 14:55:10 +0900

    [FEAT/#9] 화면 이동 및 글자 수 제한

--- app/src/main/java/com/example/momolabfe/ui/onboarding/UserInfoFragment.kt
+++ app/src/main/java/com/example/momolabfe/ui/onboarding/UserInfoFragment.kt
@@ -1,4 +1,34 @@
 package com.example.momolabfe.ui.onboarding
 
-class UserInfoFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.momolabfe.R
+import com.example.momolabfe.databinding.FragmentUserInfoBinding
+
+class UserInfoFragment : Fragment() {
+
+    private var _binding: FragmentUserInfoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AgreementFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
 }
