a1d974eb8cd1e6255fd5dc2074dd6d58de54f52f
벨라 <icomksh@gmail.com>
2025-10-30 16:22:25 +0900

    [FEAT/#17] 회차별 기록을 작성할 수 있는 버튼 카드뷰 추가

--- app/src/main/java/com/example/momolabfe/ui/record/adapter/ExchangeInfoAdapter.kt
+++ app/src/main/java/com/example/momolabfe/ui/record/adapter/ExchangeInfoAdapter.kt
@@ -3,6 +3,7 @@ package com.example.momolabfe.ui.record.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.momolabfe.R
@@ -14,10 +15,23 @@ class ExchangeInfoAdapter(
     inner class ExchangeInfoVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val exchangeNoTv: TextView = itemView.findViewById(R.id.exchange_no_tv)
         private val exchangeTimeTv: TextView = itemView.findViewById(R.id.exchange_time_tv)
+        private val addIv: ImageView = itemView.findViewById(R.id.add_iv)
 
-        fun bind(item: RecordExchangeInfo) {
-            exchangeNoTv.text = "${item.exchangeNo}회차"
-            exchangeTimeTv.text = item.exchangeTimeText
+        fun bind(position: Int) {
+            val isAddCell = position == items.size
+            if (isAddCell) {
+                exchangeNoTv.visibility = View.GONE
+                exchangeTimeTv.visibility = View.GONE
+                addIv.visibility = View.VISIBLE
+            } else {
+                val item = items[position]
+                exchangeNoTv.text = "${item.exchangeNo}회차"
+                exchangeTimeTv.text = item.exchangeTimeText
+                exchangeNoTv.visibility = View.VISIBLE
+                exchangeTimeTv.visibility = View.VISIBLE
+                addIv.visibility = View.GONE
+                itemView.setOnClickListener(null)
+            }
         }
     }
 
@@ -28,8 +42,8 @@ class ExchangeInfoAdapter(
     }
 
     override fun onBindViewHolder(holder: ExchangeInfoVH, position: Int) {
-        holder.bind(items[position])
+        holder.bind(position)
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = items.size + 1
 }
