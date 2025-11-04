4543ac8baf754a84e7c8d189494f11bd94dff5a9
벨라 <icomksh@gmail.com>
2025-11-04 11:17:07 +0900

    [FEAT/#28] 바텀바 색상 및 간격 처리

--- app/src/main/java/com/example/momolabfe/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/momolabfe/ui/main/MainActivity.kt
@@ -27,17 +27,18 @@ class MainActivity : AppCompatActivity() {
 
         initBottomNavigation()
 
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frm, LoginFragment())
                 .commit()
-
-            enableEdgeToEdge()
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
         }
     }
 
