e18135a91d6f0f30889dba5791209b0c274e9cd3
벨라 <icomksh@gmail.com>
2025-11-24 13:42:50 +0900

    [FEAT/#54] 캘린더 날짜 클릭 시 특정 기록 조회(rv 조회 문제 해결 필요)

--- app/src/main/java/com/example/momolabfe/remote/user/data/UserResponse.kt
+++ app/src/main/java/com/example/momolabfe/remote/user/data/UserResponse.kt
@@ -2,9 +2,11 @@ package com.example.momolabfe.remote.user.data
 
 import com.google.gson.annotations.SerializedName
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class MyPageResponse (
     @SerializedName("loginId") val loginId: String,
     @SerializedName("recordStartDate") val recordStartDate: LocalDate,
-    @SerializedName("recordPeriod") val recordPeriod: String
+    @SerializedName("recordPeriod") val recordPeriod: String,
+    @SerializedName("lastLoginAt") val lastLoginAt: LocalDateTime
 )
--- app/src/main/java/com/example/momolabfe/ui/consult/viewModel/ConsultViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.momolabfe.ui.consult.viewModel
-
-class ConsultViewModel {
-}
