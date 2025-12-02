5a0364c16a2271dedbd04fd7a5c86ba75f86e35d
벨라 <icomksh@gmail.com>
2025-12-02 09:56:32 +0900

    [FEAT/#93] 알림 채널 생성

--- app/src/main/java/com/example/momolabfe/application/AidiApplication.kt
+++ app/src/main/java/com/example/momolabfe/application/AidiApplication.kt
@@ -1,6 +1,10 @@
 package com.example.momolabfe.application
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -8,5 +12,27 @@ class AidiApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // 오레오 이상에서만 채널 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "momolab_morning_channel"
+            val channelName = "아침 투석 알림"
+            val descriptionText = "MOMOLAB AIDI의 아침 투석 계획 알림 채널입니다."
+
+            val importance = NotificationManager.IMPORTANCE_HIGH // 헤드업 알림용
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = descriptionText
+                enableVibration(true) // 진동
+                enableLights(true) // LED (지원 단말)
+                setShowBadge(true)
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
