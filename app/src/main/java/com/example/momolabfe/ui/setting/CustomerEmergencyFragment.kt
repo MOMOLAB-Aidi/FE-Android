b35c395185c5ca3075565394f488a7825e51f69f
벨라 <icomksh@gmail.com>
2025-12-03 09:24:18 +0900

    [REFACTOR/#106] 통화 앱 연결

--- app/src/main/java/com/example/momolabfe/ui/setting/CustomerEmergencyFragment.kt
+++ app/src/main/java/com/example/momolabfe/ui/setting/CustomerEmergencyFragment.kt
@@ -1,9 +1,11 @@
 package com.example.momolabfe.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.example.momolabfe.databinding.FragmentCustomerEmergencyBinding
 
@@ -24,6 +26,11 @@ class CustomerEmergencyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.emergencyCallBtn.setOnClickListener {
+            val phone = binding.emergencyPhoneTv.text.toString()
+            val intent = Intent(Intent.ACTION_DIAL, "tel:$phone".toUri())
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
